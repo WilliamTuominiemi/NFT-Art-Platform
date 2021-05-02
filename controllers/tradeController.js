a24886dbf87d9d5e4b7652926cc86ff76b8a9512
williamtuominiemi <william.m.tuominiemi@gmail.com>
2021-05-02 16:25:50 +0300

    can't send trade to yourself

--- controllers/tradeController.js
+++ controllers/tradeController.js
@@ -6,39 +6,47 @@ let BlockChain = require('../src/blockChain')
 let BlockChainModel = require('../src/database/model')
 
 const trade = (req, res) => {
-    // console.log(req.params.id)
-    User.find({ googleId: req.params.id }).then((receiver) => {
-        Drawing.find({ googleId: req.user.googleId }).then((user_drawings) => {
-            Drawing.find({ googleId: req.params.id }).then((receiver_drawings) => {
-                // console.log(req.user)
-                // console.log(receiver)
-                res.render('trade', {
-                    title: 'Trade',
-                    user: req.user,
-                    receiver: receiver[0],
-                    user_drawings,
-                    receiver_drawings,
+    if(req.user.googleId === req.params.id) {
+        res.redirect('/user/'+req.params.id)
+    }   else    {
+         // console.log(req.params.id)
+        User.find({ googleId: req.params.id }).then((receiver) => {
+            Drawing.find({ googleId: req.user.googleId }).then((user_drawings) => {
+                Drawing.find({ googleId: req.params.id }).then((receiver_drawings) => {
+                    // console.log(req.user)
+                    // console.log(receiver)
+                    res.render('trade', {
+                        title: 'Trade',
+                        user: req.user,
+                        receiver: receiver[0],
+                        user_drawings,
+                        receiver_drawings,
+                    })
                 })
             })
         })
-    })
+    } 
 }
 
 const trade_post = (req, res) => {
-    console.log(req.body)
+    if(req.user.googleId === req.params.id) {
+        res.redirect('/user/'+req.params.id)
+    }   else    {
+        console.log(req.body)
 
-    const body = {
-        sender_id: req.user.googleId,
-        receiver_id: req.params.id,
-        sender_drawings: req.body.sender_drawings,
-        receiver_drawings: req.body.receiver_drawings,
-    }
-
-    let trade = new Trade(body)
-    trade.save().then((result) => {
-        console.log(result)
-        res.redirect('/trade/s')
-    })
+        const body = {
+            sender_id: req.user.googleId,
+            receiver_id: req.params.id,
+            sender_drawings: req.body.sender_drawings,
+            receiver_drawings: req.body.receiver_drawings,
+        }
+    
+        let trade = new Trade(body)
+        trade.save().then((result) => {
+            console.log(result)
+            res.redirect('/trade/s')
+        })
+    }  
 }
 
 const trades = (req, res) => {
