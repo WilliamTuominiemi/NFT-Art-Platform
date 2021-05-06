c738f5b644e9c76dd7cdbb0b8789c9b9c8fda0fc
WilliamTuominiemi <william.m.tuominiemi@gmail.com>
2021-05-06 14:51:24 +0300

    better filters

---
+++ public/javascript/filters.js
@@ -0,0 +1,8 @@
+function toggle() {
+    var x = document.getElementById('filter')
+    if (x.style.display === 'none') {
+        x.style.display = 'block'
+    } else {
+        x.style.display = 'none'
+    }
+}
