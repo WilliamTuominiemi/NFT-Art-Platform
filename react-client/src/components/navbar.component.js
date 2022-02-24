10a138b6d9ae8e65d6e0b854c893b9336e4b5041
williamtuominiemi <william.m.tuominiemi@gmail.com>
2022-02-24 11:10:02 +0200

    react navbar

--- react-client/src/components/navbar.component.js
+++ react-client/src/components/navbar.component.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import {  Navbar, Container, Nav, NavDropdown } from 'react-bootstrap'
 
-export default class Navbar extends Component {
+export default class NavbarComponent extends Component {
 
   render() {
     return (
-      <h1>navbar</h1>
+        <Navbar bg="light" expand="lg">
+            <Container>
+            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                    <Nav.Link href="#home">Home</Nav.Link>
+                    <Nav.Link href="#link">Link</Nav.Link>
+                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     )
   }
 }
