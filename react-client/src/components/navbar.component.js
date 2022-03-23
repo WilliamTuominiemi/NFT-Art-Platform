30f6b8a2fe0801a77a11b11b232436d44a75a57d
williamtuominiemi <william.m.tuominiemi@gmail.com>
2022-03-23 20:28:58 +0200

    Replaced placeholder nav

--- react-client/src/components/navbar.component.js
+++ react-client/src/components/navbar.component.js
@@ -8,19 +8,14 @@ export default class NavbarComponent extends Component {
     return (
         <Navbar bg="light" expand="lg">
             <Container>
-            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar.Brand href="#home">BayyNet</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
                     <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#link">Link</Nav.Link>
-                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                        </NavDropdown>
+                    <Nav.Link href="#profile">Profile</Nav.Link>
+                    <Nav.Link href="#settings">Settings</Nav.Link>
+                    <Nav.Link href="#login">Log in</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
