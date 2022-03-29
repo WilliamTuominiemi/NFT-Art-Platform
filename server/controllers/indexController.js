d49aa5e24397be71dce670dd11a6fe5a832bb159
WilliamTuominiemi <william.m.tuominiemi@gmail.com>
2022-03-29 15:52:13 +0300

    send image src from database

--- server/controllers/indexController.js
+++ server/controllers/indexController.js
@@ -2,11 +2,12 @@ const Drawing = require('../models/image')
 
 const main = (req, res) => {
     console.log('request')
-    // Drawing.find()
-    //     .sort({ likes: -1 })
-    //     .then((result) => {
-    //         console.log(result)
-    //     })
+    Drawing.find()
+        .sort({ likes: -1 })
+        .then((result) => {
+            console.log(result[0].src)
+            res.send(result[0].src)
+        })
 }
 
 module.exports = {
