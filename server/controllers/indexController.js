eaa17f70b42c0715e8711e5e88280a1321ffda0b
WilliamTuominiemi <william.m.tuominiemi@gmail.com>
2022-03-31 10:57:46 +0300

    display all images in database

--- server/controllers/indexController.js
+++ server/controllers/indexController.js
@@ -5,8 +5,7 @@ const main = (req, res) => {
     Drawing.find()
         .sort({ likes: -1 })
         .then((result) => {
-            console.log(result[0].src)
-            res.send(result[0].src)
+            res.send(result)
         })
 }
 
