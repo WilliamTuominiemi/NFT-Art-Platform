37caa81084b58aa3683530cc485dbd53d9fdea4c
williamtuominiemi <william.m.tuominiemi@gmail.com>
2022-06-12 10:41:31 +0300

    user route

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const app = express()
 const port = 8080
 
 const index = require('./routes/index')
+const user = require('./routes/user')
 
 // Load config
 dotenv.config({ path: './config/.env' })
@@ -17,6 +18,7 @@ connectDB()
 app.use(cors())
 
 app.use('/', index)
+app.use('/user', user)
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
