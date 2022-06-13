e7d4bf031adc5c74ba9d5a54a8d2a147992a3588
williamtuominiemi <william.m.tuominiemi@gmail.com>
2022-06-13 11:15:36 +0300

    fixed navbar links

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -10,10 +10,10 @@ export default function Menu() {
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#profile">Profile</Nav.Link>
-                    <Nav.Link href="#settings">Settings</Nav.Link>
-                    <Nav.Link href="#login">Log in</Nav.Link>
+                    <Nav.Link href="/">Home</Nav.Link>
+                    <Nav.Link href="/profile">Profile</Nav.Link>
+                    <Nav.Link href="/settings">Settings</Nav.Link>
+                    <Nav.Link href="/login">Log in</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
