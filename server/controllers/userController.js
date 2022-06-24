786818da6028e2b7395bf19a764ad833a14adf92
williamtuominiemi <william.m.tuominiemi@gmail.com>
2022-06-24 22:43:55 +0300

    own profile backend

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -3,16 +3,16 @@ const User = require('../models/User')
 const getProfile = async (req, res) => {
     console.log('get user')
     console.log(req.user)
-    // try {
-    //     if(typeof req.user != 'undefined') {
-    //         const profile = await User.find({googleId: req.user.googleId})
-    //         res.json(profile[0])
-    //     }  else {
-    //         res.json('undefined')
-    //     }
-    // } catch (err) {
-    //     console.log(err)
-    // }
+    try {
+        if(typeof req.user != 'undefined') {
+            const profile = await User.find({googleId: req.user.googleId})
+            res.json(profile[0])
+        }  else {
+            res.json('undefined')
+        }
+    } catch (err) {
+        console.log(err)
+    }
 }
 
 const getProfileById = async (req, res) => {
