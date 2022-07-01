c5bb0d0a15ba5f85a6d352d9840eb8f44f260e07
williamtuominiemi <william.m.tuominiemi@gmail.com>
2022-07-02 00:18:25 +0300

    fixing mouse pointer position

--- client/src/components/hooks.js
+++ client/src/components/hooks.js
@@ -11,9 +11,23 @@ export function useOnDraw() {
 
     function initMouseMoveListener() {
         const mouseMoveListener = (e) => {
-            console.log({x:e.clientX, y:e.clientY})
+            const point = computePointInCanvas(e.clientX, e.clientY)
+            console.log(point)
         }
         window.addEventListener("mousemove", mouseMoveListener)
+    }
+
+    function computePointInCanvas(clientX, clientY) {
+        if(canvasRef.current) {
+            const boundingRect = canvasRef.current.getBoundingClientRect()
+            return {
+                x:clientX-boundingRect.left,
+                y:clientY-boundingRect.top
+            } 
+        }   else    {
+            return null
+        }
+       
     }
 
     return setCanvasRef
