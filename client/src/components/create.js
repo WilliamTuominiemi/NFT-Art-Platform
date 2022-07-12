abde93397003a1eb78c01b9d94bf45d95aaabd9a
williamtuominiemi <william.m.tuominiemi@gmail.com>
2022-07-12 11:59:38 +0300

    thickness selector

--- client/src/components/create.js
+++ client/src/components/create.js
@@ -2,13 +2,13 @@ import React, { useEffect, useState } from 'react'
 
 import Canvas from './canvas'
 
-import {uploadButton} from './hooks'
+import {uploadButton, changeNewColor} from './hooks'
 
 import { BrowserRouter as Router, useParams, Link, useNavigate  } from 'react-router-dom'
 
 import { HuePicker   } from 'react-color'
 
-import {changeNewColor} from './hooks'
+import { FormGroup, FormControl } from 'react-bootstrap'
 
 export default function Create() {
     let navigate = useNavigate();
@@ -21,6 +21,7 @@ export default function Create() {
     }
 
     let [color, setColor] = useState([])
+    let [thickness, setThickness] = useState([])
 
     function changeColor(pickedColor) {
         setColor(pickedColor)
@@ -35,6 +36,16 @@ export default function Create() {
             />
             <br />
             <HuePicker color={color} onChange={changeColor}/>
+            <FormGroup>
+                <FormControl 
+                    type='number'
+                    name='thickness' 
+                    placeholder='thiccness' 
+                    defaultValue='5'
+                    onChange = { (event) => { console.log(event.target.value) } }
+                    min="0" max="50" step="0.5"
+                    />
+            </FormGroup>
             <button onClick={buttonClick}>UPLOAD</button>
         </div>     
     )
