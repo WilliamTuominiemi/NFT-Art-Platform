605cd53f7fbc9ea8591e4798c27b4514f95383e6
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2023-04-02 16:18:00 +0300

    Sort imports

--- src/server/api/routers/example.ts
+++ src/server/api/routers/example.ts
@@ -1,10 +1,9 @@
-import { z } from "zod";
-
 import {
   createTRPCRouter,
   protectedProcedure,
   publicProcedure,
 } from "@/server/api/trpc";
+import { z } from "zod";
 
 export const exampleRouter = createTRPCRouter({
   hello: publicProcedure
