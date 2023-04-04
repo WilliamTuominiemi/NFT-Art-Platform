4804960bd079387206de2de2bdc8b01264fd8f0d
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2023-04-05 00:21:54 +0300

    Improve post card

--- src/components/post-card.tsx
+++ src/components/post-card.tsx
@@ -1,7 +1,14 @@
 import { Button } from "@/components/ui/button";
 import type { Post, User } from "@prisma/client";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
 import { Heart, MessageCircle } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useState } from "react";
+
+dayjs.extend(relativeTime);
 
 interface PostCardProps {
   post: Post & {
@@ -10,6 +17,9 @@ interface PostCardProps {
 }
 
 export const PostCard = ({ post }: PostCardProps) => {
+  const router = useRouter();
+  const [isLiked, setIsLiked] = useState(false);
+
   return (
     <div className="group rounded-md border border-slate-200 shadow-md hover:shadow-lg dark:border-slate-800">
       <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-t-md border-b border-slate-200 dark:border-slate-800">
@@ -22,18 +32,34 @@ export const PostCard = ({ post }: PostCardProps) => {
         />
       </div>
       <div className="flex flex-col space-y-4 p-4">
-        <div className="flex flex-row justify-between text-sm">
-          <p className="truncate font-medium underline-offset-4 hover:cursor-pointer hover:underline">
+        <p className="truncate text-sm">
+          <Link
+            className="font-medium underline-offset-4 hover:cursor-pointer hover:underline"
+            href={`/user/${post.user.id}`}
+          >
             {post.user.name}
-          </p>
-          <p className="text-slate-600 dark:text-slate-400">1 day ago</p>
-        </div>
-        <div className="flex flex-row space-x-3">
-          <Button size="sm" variant="ghost">
-            <Heart />
+          </Link>
+          <span className="text-slate-600 dark:text-slate-400">
+            {` ${"·"} ${dayjs(post.createdAt).fromNow()}`}
+          </span>
+        </p>
+        <div className="flex flex-row space-x-2">
+          <Button
+            size="sm"
+            variant="ghost"
+            onClick={() => setIsLiked((prev) => !prev)}
+          >
+            <Heart
+              className="text-red-400"
+              fill={isLiked ? "#f87171" : "transparent"}
+            />
           </Button>
-          <Button size="sm" variant="ghost">
-            <MessageCircle />
+          <Button
+            size="sm"
+            variant="ghost"
+            onClick={() => router.push(`/post/${post.id}`)}
+          >
+            <MessageCircle className="text-blue-400" />
           </Button>
         </div>
       </div>
