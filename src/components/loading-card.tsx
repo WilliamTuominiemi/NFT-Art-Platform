f61fddefdf3ac7d5b5a7427c1460a6bc4c87f188
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2023-04-04 23:47:33 +0300

    Add loading state to feed

---
+++ src/components/loading-card.tsx
@@ -0,0 +1,18 @@
+export const LoadingCard = () => {
+  return (
+    <div className="rounded-md border border-slate-200 shadow-md dark:border-slate-800">
+      <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-t-md border-b border-slate-200 dark:border-slate-800">
+        <div className="animate-pulse bg-slate-200 dark:bg-slate-700" />
+      </div>
+      <div className="flex flex-col space-y-4 p-4">
+        <div className="flex flex-row justify-between text-sm">
+          <div className="h-3 w-1/2 animate-pulse rounded-full bg-slate-200 dark:bg-slate-700" />
+          <div className="h-3 w-1/3 animate-pulse rounded-full bg-slate-200 dark:bg-slate-700" />
+        </div>
+        <div className="flex flex-row space-x-3">
+          <div className="h-6 w-2/3 animate-pulse rounded-full bg-slate-200 dark:bg-slate-700" />
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/components/post-card.tsx
+++ src/components/post-card.tsx
@@ -11,24 +11,24 @@ interface PostCardProps {
 
 export const PostCard = ({ post }: PostCardProps) => {
   return (
-    <div className="rounded-lg border border-slate-200 shadow-md hover:shadow-lg">
-      <Image
-        src={post.image}
-        height={500}
-        width={500}
-        alt="Post image"
-        className="rounded-t-lg border-b border-slate-200 object-cover object-top"
-      />
-      <div className="p-4">
-        <div className="flex flex-row justify-between">
-          <p className="truncate text-sm font-medium underline-offset-4 hover:cursor-pointer hover:underline">
+    <div className="group rounded-md border border-slate-200 shadow-md hover:shadow-lg dark:border-slate-800">
+      <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-t-md border-b border-slate-200 dark:border-slate-800">
+        <Image
+          src={post.image}
+          height={500}
+          width={500}
+          alt="Post image"
+          className="object-cover object-top transition-all duration-500 group-hover:scale-105"
+        />
+      </div>
+      <div className="flex flex-col space-y-4 p-4">
+        <div className="flex flex-row justify-between text-sm">
+          <p className="truncate font-medium underline-offset-4 hover:cursor-pointer hover:underline">
             {post.user.name}
           </p>
-          <div className="flex flex-col text-sm">
-            <p className="text-slate-600 dark:text-slate-400">1 day ago</p>
-          </div>
+          <p className="text-slate-600 dark:text-slate-400">1 day ago</p>
         </div>
-        <div className="mt-4 flex flex-row space-x-3">
+        <div className="flex flex-row space-x-3">
           <Button size="sm" variant="ghost">
             <Heart />
           </Button>
